4bfaae92901e43f01e2b6bf8d45a7870402d5948
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-03-02 00:31:32 +0500

    added slider in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import Image from "next/image";
 import Button from "./Button";
-
+import { Navigation, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 const Hero = () => {
     return (
         <>
-            <section className="bg-[#D7DDDF] flex items-center md:justify-between">
+            {/* <section className="bg-[#D7DDDF] flex items-center md:justify-between">
                 <Image
                     src="/icons/angle-left.png"
                     width={60}
@@ -36,7 +42,69 @@ const Hero = () => {
                     height={60}
                     alt="angle-right"
                 />
-            </section>
+            </section> */}
+            <Swiper
+                className="mySwiper md:h-[55vh]"
+                modules={[Navigation, Scrollbar, A11y, Autoplay]}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                scrollbar={{ draggable: true }}
+            >
+                <SwiperSlide className="bg-[#D7DDDF] flex items-center">
+                    <div className="h-full  flex flex-col justify-between items-center md:w-[60vw] mx-auto md:flex-row-reverse md:gap-12">
+
+                        <div>
+                            <Image src="/images/hero-img.svg" width={500} height={500} alt="hero-img" className="w-full lg:pl-12 h-[60vh]" />
+                        </div>
+
+                        <div className="flex flex-col items-center justify-between gap-6 py-10 md:justify-center md:items-start md:gap-5 lg:gap-8">
+                            <h1 className="font-light uppercase text-center text-5xl md:text-start md:text-4xl md:w-[25vw] lg:text-5xl">
+                                Watches  <br />That Define You.
+                            </h1>
+                            <Button type="button" title="Explore Watch" variant="bg-[#272727]" />
+                        </div>
+
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide className="bg-[#cdc9b3] flex items-center ">
+                    <div className="h-full flex flex-col justify-between items-center md:w-[60vw] mx-auto md:flex-row-reverse md:gap-12">
+
+                        <div className="">
+                            <Image src="/images/hero-img2.png" width={500} height={500} alt="hero-img" className="object-cover w-full h-[60vh] lg:pl-12 " />
+                        </div>
+
+                        <div className="flex flex-col items-center justify-between gap-6 py-10 md:justify-center md:items-start md:gap-5 lg:gap-8">
+                            <h1 className="font-light uppercase text-center text-5xl md:text-start md:text-4xl md:w-[25vw] lg:text-5xl">
+                                Smartphones <br /> for Every Lifestyle.                            </h1>
+                            <Button type="button" title="Explore Smartphones" variant="bg-[#272727]" />
+                        </div>
+
+                    </div>
+                </SwiperSlide>
+
+
+                <SwiperSlide className="bg-[#dbc9d3] flex items-center ">
+                    <div className="h-full flex flex-col justify-between items-center md:w-[60vw] mx-auto md:flex-row-reverse md:gap-12">
+
+                        <div className="">
+                            <Image src="/images/hero-img3.png" width={500} height={500} alt="hero-img" className="object-contain w-full h-[60vh] lg:pl-12 " />
+                        </div>
+
+                        <div className="flex flex-col items-center justify-between gap-6 py-10 md:justify-center md:items-start md:gap-5 lg:gap-8">
+                            <h1 className="font-light uppercase text-center text-5xl md:text-start md:text-4xl md:w-[25vw] lg:text-5xl">
+                                Explore Latest <br />Watch Collections.                          </h1>
+                            <Button type="button" title="Explore More" variant="bg-[#272727]" />
+                        </div>
+
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </>
     );
 };
